b77a128043e893f9b671ea0904e24c55604c445f
Spladed <1310140596@qq.com>
2019-12-15 19:16:39 +0800

    创建项目

---
+++ src/Display/display.java
@@ -0,0 +1,43 @@
+package Display;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class display {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					display window = new display();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public display() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 842, 619);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
